8ccefc60195bb61d197883e6da2828a853955637
Web admin <webadmin@ph-playground.store>
2022-10-01 11:24:12 +0200

    Empty spaces and typo cleanup.

--- js/swiper_formatter.js
+++ js/swiper_formatter.js
@@ -12,9 +12,9 @@
   Drupal.behaviors.nkToolsSwiper = {
 
     attach: function(context, settings) {
-  
+
       var self = this;
-  
+
       var swiper_formatter_settings = settings.swiper_formatter || null;
 
       if (swiper_formatter_settings && $.type(swiper_formatter_settings.swipers) !== 'undefined') { 
@@ -22,30 +22,30 @@
         var swipers = {};
 
         once('swiperFormaterInit', '.swiper-container', context).forEach(function(swiperContainer) {
-	     
-	      if (swiperContainer.id) {
+
+	        if (swiperContainer.id) {
             var swiperSettings = swiper_formatter_settings.swipers[swiperContainer.id];
             if (typeof swiperSettings === 'object' && typeof Swiper !== 'undefined') {
-	          
+	
               if (swiperSettings.pagination.type === 'progressbar') {
-	            $(swiperContainer).addClass('progressbar');  
+	              $(swiperContainer).addClass('progressbar');
               }
-              
-	          swipers[swiperContainer.id] = new Swiper('#' + swiperContainer.id, swiperSettings);
-                
+
+	            swipers[swiperContainer.id] = new Swiper('#' + swiperContainer.id, swiperSettings);
+
               if (swipers[swiperContainer.id]) {
-                
+
                 // Swiper's slideChangeTransitionEnd event.
                 swipers[swiperContainer.id].on('slideChangeTransitionEnd', function(e) {
                   self.showHidden(this);
                 });
-                
+
                 // A custom links (anywhere on the page) that trigger swiper slides.
                 self.registerTriggers(swipers[swiperContainer.id], $(context).find('.swiper-trigger'), context, settings); 
               }
+	          }
 	        }
-	      }
-	    });    
+	      });    
       }
     },
 
@@ -64,11 +64,11 @@
     registerTriggers: function(swiper, triggers, context, settings) {
       triggers.once('swiperSwipe').each(function() {  
         $(this).on('click', function(e) {
-       
+
           $(context).find('.swiper-trigger').each(function(i, sibling) {
             $(sibling).removeClass('active');
           });
-       
+
           setTimeout(function() {
             $(e.currentTarget).addClass('active');
           }, 1);
