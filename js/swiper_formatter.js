aed0fb76aad381e2c05e5bbd3237996cbd2231d2
nk- <nenadkesic@gmail.com>
2024-10-20 17:41:27 +0200

    #3472606 by ryankavalsky. Also, eslint and prettify JS code.

--- js/swiper_formatter.js
+++ js/swiper_formatter.js
@@ -4,71 +4,76 @@
  */
 
 (function ($, Drupal, drupalSettings, once) {
-
   'use strict';
 
   Drupal.swiper_formatter = Drupal.swiper_formatter || {};
 
-   /**
-    * Drupal.behaviors implementation.
-    *
-    * Register and initialise all Swiper instances on the page.
-    *
-    */
-   Drupal.behaviors.nkToolsSwiper = {
-
-    attach: function(context, settings) {
-
+  /**
+   * Drupal.behaviors implementation.
+   *
+   * Register and initialise all Swiper instances on the page.
+   *
+   */
+  Drupal.behaviors.swiperFormatter = {
+    attach: function (context, settings) {
       var self = this;
 
       var swiper_formatter_settings = settings.swiper_formatter || null;
 
-      if (swiper_formatter_settings && $.type(swiper_formatter_settings.swipers) !== 'undefined') { 
-
+      if (swiper_formatter_settings && $.type(swiper_formatter_settings.swipers) !== 'undefined') {
         var swipers = {};
 
-        once('swiperFormaterInit', '.swiper-container', context).forEach(function(swiperContainer) {
+        once('swiperFormaterInit', '.swiper-container', context).forEach(
+          function (swiperContainer) {
+            if (swiperContainer.id) {
+              var swiperSettings = swiper_formatter_settings.swipers[swiperContainer.id];
+              if (typeof swiperSettings === 'object' && typeof Swiper !== 'undefined') {
+                if (swiperSettings.pagination.type === 'progressbar') {
+                  $(swiperContainer).addClass('progressbar');
+                }
 
-	        if (swiperContainer.id) {
-            var swiperSettings = swiper_formatter_settings.swipers[swiperContainer.id];
-            if (typeof swiperSettings === 'object' && typeof Swiper !== 'undefined') {
-	
-              if (swiperSettings.pagination.type === 'progressbar') {
-	              $(swiperContainer).addClass('progressbar');
+                swipers[swiperContainer.id] = new Swiper('#' + swiperContainer.id, swiperSettings);
+
+                if (swipers[swiperContainer.id]) {
+                  // Swiper's slideChangeTransitionEnd event.
+                  // @todo: Make this as option in entity configuration form.
+                  // swipers[swiperContainer.id].on('slideChangeTransitionEnd', function(e) {
+                  //   self.showHidden(this);
+                  // });
+
+                  // A custom links (anywhere on the page) that trigger swiper slides.
+                  self.registerTriggers(
+                    swipers[swiperContainer.id],
+                    $(context).find('.swiper-trigger'),
+                    context,
+                    settings
+                  );
+
+                  // Send swipers to the front-end.
+                  drupalSettings.swipers = swipers;
+                }
               }
-
-	            swipers[swiperContainer.id] = new Swiper('#' + swiperContainer.id, swiperSettings);
-
-              if (swipers[swiperContainer.id]) {
-
-                // Swiper's slideChangeTransitionEnd event.
-                // @todo: Make this as option in entity configuration form.
-                // swipers[swiperContainer.id].on('slideChangeTransitionEnd', function(e) {
-                //   self.showHidden(this);
-                // });
-
-                // A custom links (anywhere on the page) that trigger swiper slides.
-                self.registerTriggers(swipers[swiperContainer.id], $(context).find('.swiper-trigger'), context, settings); 
-              }
-	          }
-	        }
-	      });    
+            }
+          }
+        );
       }
     },
 
     /**
      * Show's any ".hidden" elements on a new slide.
      *
-     * @param object swiper
+     * @param {object} swiper
      *  Current Swiper object.
      */
-    showHidden: function(swiper) {
+    showHidden: function (swiper) {
       if (swiper && swiper.slides.length > 0) {
-        $.each(swiper.slides, function(index, slide) {
-          if (index == swiper.activeIndex) {
-            $(slide).find('.hidden').each(function(d, hidden) {
-              $(hidden).removeClass('hidden');
-            });
+        $.each(swiper.slides, function (index, slide) {
+          if (index === swiper.activeIndex) {
+            $(slide)
+              .find('.hidden')
+              .each(function (d, hidden) {
+                $(hidden).removeClass('hidden');
+              });
           }
         });
       }
@@ -77,9 +82,9 @@
     /**
      * Run sliding from anywhere, with some markup attributes defined.
      *
-     * @param object swiper
+     * @param {object} swiper
      *  Current Swiper object.
-     * @param array triggers
+     * @param {array} triggers
      *  Array with trigger elements/objects.
      * @code
      *  <ul>
@@ -88,15 +93,17 @@
      *  </ul>
      * @endcode
      */
-    registerTriggers: function(swiper, triggers) {
-      triggers.each(function(trigger) {
-        $(this).on('click', function(e) {
-
+    registerTriggers: function (swiper, triggers) {
+      triggers.each(function (trigger) {
+        $(this).on('click', function (e) {
           // Take care of siblings' active class.
           if ($(this).parent().siblings().length) {
-            $(this).parent().siblings().each(function(i, sibling) {
-              $(sibling).find('.swiper-trigger').removeClass('active');
-            });
+            $(this)
+              .parent()
+              .siblings()
+              .each(function (i, sibling) {
+                $(sibling).find('.swiper-trigger').removeClass('active');
+              });
           }
 
           $(e.currentTarget).addClass('active');
@@ -105,8 +112,7 @@
           swiper.slideTo(index);
           return false;
         });
-      });   
+      });
     }
   };
-
 })(jQuery, Drupal, drupalSettings, once);
