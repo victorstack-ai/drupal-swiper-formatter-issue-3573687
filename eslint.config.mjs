77ea537c4c082e80771441f6e8263ae36498c0d3
nk- <nenadkesic@gmail.com>
2024-10-29 22:45:43 +0100

    Improve CI config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module"
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
 ];
